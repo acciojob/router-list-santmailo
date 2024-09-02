71129f6e649f94934fb79bfc8e013e2c59870be4
santmailo <iamsunil006@gmail.com>
2024-09-02 11:08:38 +0530

    attempt 2

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,17 +3,20 @@ import { Link } from "react-router-dom";
 
 function ItemList() {
   return (
-    <ul>
-      <Link to="/list/1">
-        <li>Item 1</li>
-      </Link>
-      <Link to="/list/2">
-        <li>Item 2</li>
-      </Link>
-      <Link to="/list/3">
-        <li>Item 3</li>
-      </Link>
-    </ul>
+    <>
+      <h1>Item List</h1>
+      <ul>
+        <Link to="/list/1">
+          <li>Item 1</li>
+        </Link>
+        <Link to="/list/2">
+          <li>Item 2</li>
+        </Link>
+        <Link to="/list/3">
+          <li>Item 3</li>
+        </Link>
+      </ul>
+    </>
   );
 }
 
